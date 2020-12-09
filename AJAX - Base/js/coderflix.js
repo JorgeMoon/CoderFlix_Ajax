337c0a5b2d53d9d2820c91769277619b6550bb41
Damian Gamarra <djmgamarra@gmail.com>
2020-12-09 20:50:50 -0300

    primer commit

---
+++ AJAX - Base/js/coderflix.js
@@ -0,0 +1,24 @@
+let HTMLCard = "";
+let contenidoJSON = []
+ //Template Literals o Template Strings ``
+ // ${}
+ //VENTAJAS: Concatenar o interpolar Strings.
+ //          Basta de caracteres de escape cuando usamos comillas simples
+ //          Armar strings en varias líneas de manera más cómoda (sin concatenación!)
+ //          https://latteandcode.medium.com/javascript-como-funcionan-las-comillas-invertidas-88c7fb209e9e
+ //          https://uniwebsidad.com/tutoriales/las-nuevas-cadenas-de-texto-de-javascript-6
+
+
+ 
+function cargoContenidoStreaming() {
+
+}
+
+   setTimeout(() => {
+      cargoContenidoStreaming()
+         $('#contenido').fadeIn("slow")
+   }, 2000)
+
+function verDetalle(i) {
+
+}
